68fa10f10a1bddce66c9b6865755f48ded6259d3
Anthony Di Tomasso <anthony.ditomasso@outlook.com>
2020-01-30 15:18:45 +0100

    renamming

--- SOTabBar/Classes/SOTabBarSetting.swift
+++ SOTabBar/Classes/SOTabBarSetting.swift
@@ -12,18 +12,20 @@ public struct SOTabBarSetting {
     
     public static var selectedColor: UIColor = UIColor(red: 250/255, green: 51/255, blue: 24/255, alpha: 1)
     public static var backgroundColor: UIColor = UIColor.white
+    
     public static var circleSize = CGSize(width: 65, height: 65)
     public static var imageSize: CGFloat = 25
+    public static var selectedImageSize: CGFloat = 30
+    
+    public static var animationDurationTime: Double = 0.4
     
     public static var textColor: UIColor = .blue
     public static var textFont: UIFont = UIFont.systemFont(ofSize: 12, weight: UIFont.Weight.semibold)
     public static var textFontSelected: UIFont = UIFont.systemFont(ofSize: 12, weight: UIFont.Weight.bold)
+    
     public static var shadowColor = UIColor.lightGray.cgColor
     public static var shadowOpacity: Float = 0.2
     public static var shadowRadius: CGFloat = 7
     public static var shadowHeight: CGFloat = 4
-    
-    public static var selectedImage: CGFloat = 20
-    public static var animationDurationTime: Double = 0.4
     
 }
